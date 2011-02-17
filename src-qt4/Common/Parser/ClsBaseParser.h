f363fe501f498f993cdc19c688b0d2727f5a2f0b
Jesus Sanchez-Palencia <jesus@webkit.org>
2011-02-17 13:46:28 +0100

    QtXML WIP: BaseParser
    Signed-off-by: Jesus Sanchez-Palencia <jesus@webkit.org>

--- src-qt4/Common/Parser/ClsBaseParser.h
+++ src-qt4/Common/Parser/ClsBaseParser.h
@@ -14,18 +14,21 @@
 #include <string>
 #include <list>
 
-#include <framework/MemBufInputSource.hpp>
-#include <parsers/XercesDOMParser.hpp>
-#include <dom/DOM.hpp>
-#include <sax/HandlerBase.hpp>
-#include <util/XMLString.hpp>
-#include <util/PlatformUtils.hpp>
+#include <QtXml>
+#include <QString>
+
+//#include <framework/MemBufInputSource.hpp>
+//#include <parsers/XercesDOMParser.hpp>
+//#include <dom/DOM.hpp>
+//#include <sax/HandlerBase.hpp>
+//#include <util/XMLString.hpp>
+//#include <util/PlatformUtils.hpp>
 
 #include "../Helper/iqrUtils.h"
 
 #include "ClsSysFileParserException.h"
 
-using namespace xercesc;
+//using namespace xercesc;
 using namespace std;
 
 namespace iqrcommon {
@@ -36,15 +39,16 @@ namespace iqrcommon {
 	virtual ~ClsBaseParser(){};
 
     protected:
-	inline string getAttributeValue(DOMNode *dn, string strAttributeName, bool bRequired) {
+        inline string getAttributeValue(QDomNode *dn, string strAttributeName, bool bRequired) {
 #ifdef DEBUG_CLSBASEPARSER
-	    cout << "ClsSysFileParser::getAttributeValue(DOMNode *dn, string strAttributeName, bool bRequired)" << endl;
+            cout << "ClsSysFileParser::getAttributeValue(QDomNode *dn, string strAttributeName, bool bRequired)" << endl;
 #endif
 
 	    string strValue;
-	    DOMNode *dnTemp = dn->getAttributes()->getNamedItem(XMLString::transcode(strAttributeName.c_str()));
+            QDomNode dnTemp = dn->attributes().namedItem(QString::fromStdString(strAttributeName));
+            //DOMNode *dnTemp = dn->getAttributes()->getNamedItem(XMLString::transcode(strAttributeName.c_str()));
 
-	    if(dnTemp == NULL){
+            if(dnTemp.isNull()){
 		if(bRequired){
 		    strValue = "";
 		    ClsSysFileParserException clsSysFileParserException(ClsSysFileParserException::ATTRIBUTE_NOT_FOUND, strAttributeName);
@@ -53,7 +57,7 @@ namespace iqrcommon {
 		    strValue = "";
 		}
 	    } else {
-		strValue = XMLString::transcode(dnTemp->getNodeValue());
+                strValue = dnTemp.nodeValue().toStdString();
 	    }
 	    return strValue;
 	}
