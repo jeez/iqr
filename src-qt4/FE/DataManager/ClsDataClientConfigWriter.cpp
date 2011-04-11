68585454c0943c1bf76ad7c0fc7a0d785c632de6
Jesus Sanchez-Palencia <jesus@webkit.org>
2011-04-11 17:26:43 +0200

    Finished ClsDataClientConfigWriter Signed-off-by: Jesus Sanchez-Palencia <jesus@webkit.org>

--- src-qt4/FE/DataManager/ClsDataClientConfigWriter.cpp
+++ src-qt4/FE/DataManager/ClsDataClientConfigWriter.cpp
@@ -22,11 +22,10 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
     //DOMImplementation *impl = DOMImplementationRegistry::getDOMImplementation(XMLString::transcode((const char*)"Range"));
 
     QDomImplementation impl;
-    QDomDocumentType dtd = impl.ccreateDocumentType(QString.fromStdString(ConfigTagLibrary::DataManagerConfiguration()),
-                            QString.fromStdString("-//INI/iqr421"),
-                            QString.fromStdString("iqrDataManagerConfiguration.dtd"));
+    QDomDocumentType dtd = impl.createDocumentType(QString::fromStdString(ConfigTagLibrary::DataManagerConfiguration()),
+                                                    QString("-//INI/iqr421"), QString("iqrDataManagerConfiguration.dtd"));
 
-    QDomDocument ddocConfig = impl.createDocument(0, QString.fromStdString(ConfigTagLibrary::DataManagerConfiguration()), dtd);
+    QDomDocument ddocConfig = impl.createDocument(0, QString::fromStdString(ConfigTagLibrary::DataManagerConfiguration()), dtd);
 
     QDomElement delemConfig;
     delemConfig = ddocConfig.documentElement();
@@ -39,25 +38,25 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
     pair<int,int> pGeometry =  (*itConfigs).getGeometry();
 
     QDomElement delemDataClient;
-    delemDataClient = ddocConfig.createElement(QString.fromStdString(ConfigTagLibrary::DataClientTag()));
-    delemDataClient.setAttribute(QString.fromStdString(ConfigTagLibrary::TypeTag()), QString.fromStdString(strType.c_str()));
-    delemDataClient.setAttribute(QString.fromStdString(ConfigTagLibrary::IDTag()), QString.fromStdString(strID.c_str()));
+    delemDataClient = ddocConfig.createElement(QString::fromStdString(ConfigTagLibrary::DataClientTag()));
+    delemDataClient.setAttribute(QString::fromStdString(ConfigTagLibrary::TypeTag()), QString::fromStdString(strType.c_str()));
+    delemDataClient.setAttribute(QString::fromStdString(ConfigTagLibrary::IDTag()), QString::fromStdString(strID.c_str()));
     delemConfig.appendChild(delemDataClient);
 
     QDomElement delemPosition;
-    delemPosition = ddocConfig.createElement(QString.fromStdString(ConfigTagLibrary::PositionTag()));
-    delemPosition.setAttribute(QString.fromStdString(ConfigTagLibrary::PositionXTag()),
-                    QString.fromStdString(iqrUtils::int2string(pPosition.first).c_str()));
-    delemPosition.setAttribute(QString.fromStdString(ConfigTagLibrary::PositionYTag()),
-                    QString.fromStdString(iqrUtils::int2string(pPosition.second).c_str()));
+    delemPosition = ddocConfig.createElement(QString::fromStdString(ConfigTagLibrary::PositionTag()));
+    delemPosition.setAttribute(QString::fromStdString(ConfigTagLibrary::PositionXTag()),
+                    QString::fromStdString(iqrUtils::int2string(pPosition.first).c_str()));
+    delemPosition.setAttribute(QString::fromStdString(ConfigTagLibrary::PositionYTag()),
+                    QString::fromStdString(iqrUtils::int2string(pPosition.second).c_str()));
     delemDataClient.appendChild(delemPosition);
 
     QDomElement delemGeometry;
-    delemGeometry = ddocConfig.createElement(QString.fromStdString(ConfigTagLibrary::Geometry()));
-    delemGeometry.setAttribute(QString.fromStdString(ConfigTagLibrary::GeometryWidthTag()),
-                    QString.fromStdString(iqrUtils::int2string(pGeometry.first).c_str()));
-    delemGeometry.setAttribute(QString.fromStdString(ConfigTagLibrary::GeometryHeightTag()),
-                    QString.fromStdString(iqrUtils::int2string(pGeometry.second).c_str()));
+    delemGeometry = ddocConfig.createElement(QString::fromStdString(ConfigTagLibrary::Geometry()));
+    delemGeometry.setAttribute(QString::fromStdString(ConfigTagLibrary::GeometryWidthTag()),
+                    QString::fromStdString(iqrUtils::int2string(pGeometry.first).c_str()));
+    delemGeometry.setAttribute(QString::fromStdString(ConfigTagLibrary::GeometryHeightTag()),
+                    QString::fromStdString(iqrUtils::int2string(pGeometry.second).c_str()));
     delemDataClient.appendChild(delemGeometry);
 
 
@@ -68,35 +67,35 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
         string strParamName = (*itLstParameters).first;
         string strParamValue = (*itLstParameters).second;
         QDomElement delemParameter;
-        delemParameter = ddocConfig.createElement(QString.fromStdString(strParamName.c_str()));
+        delemParameter = ddocConfig.createElement(QString::fromStdString(strParamName.c_str()));
         delemDataClient.appendChild(delemParameter);
-        QDomText dtxtParamValue = ddocConfig.createTextNode(QString.fromStdString(strParamValue.c_str()));
+        QDomText dtxtParamValue = ddocConfig.createTextNode(QString::fromStdString(strParamValue.c_str()));
         delemParameter.appendChild(dtxtParamValue);
     }
 
     QDomElement delemSVD;
-    delemSVD = ddocConfig.createElement(QString.fromStdString(ConfigTagLibrary::StateVariableDisplayTag()));
+    delemSVD = ddocConfig.createElement(QString::fromStdString(ConfigTagLibrary::StateVariableDisplayTag()));
     delemDataClient.appendChild(delemSVD);
 
     list<ClsStateVariableDisplayConfig>lstSVDConfigs =  (*itConfigs).getListStateVariableDisplayConfig();
     list<ClsStateVariableDisplayConfig>::iterator itSVD;
     for(itSVD = lstSVDConfigs.begin(); itSVD != lstSVDConfigs.end(); itSVD++){
         QDomElement delemStateVariable;
-        delemStateVariable = ddocConfig.createElement(QString.fromStdString(ConfigTagLibrary::StateVariableDisplaysTag()));
+        delemStateVariable = ddocConfig.createElement(QString::fromStdString(ConfigTagLibrary::StateVariableDisplaysTag()));
         delemSVD.appendChild(delemStateVariable);
-        delemStateVariable.setAttribute(QString.fromStdString(ConfigTagLibrary::IDTag()),
-                         QString.fromStdString((*itSVD).getID().c_str()));
+        delemStateVariable.setAttribute(QString::fromStdString(ConfigTagLibrary::IDTag()),
+                         QString::fromStdString((*itSVD).getID().c_str()));
 
 /*
         delemStateVariable->setAttribute(XMLString::transcode(ConfigTagLibrary::TypeTag()),
                          XMLString::transcode((*itSVD).getItemType().c_str()));
 */
 
-        delemStateVariable.setAttribute(QString.fromStdString(ConfigTagLibrary::ItemIDTag()),
-                         QString.fromStdString((*itSVD).getItemID().c_str()));
+        delemStateVariable.setAttribute(QString::fromStdString(ConfigTagLibrary::ItemIDTag()),
+                         QString::fromStdString((*itSVD).getItemID().c_str()));
 
-        delemStateVariable.setAttribute(QString.fromStdString(ConfigTagLibrary::SelectedIndicesTag()),
-                         QString.fromStdString((*itSVD).getSelectedIndices().c_str()));
+        delemStateVariable.setAttribute(QString::fromStdString(ConfigTagLibrary::SelectedIndicesTag()),
+                         QString::fromStdString((*itSVD).getSelectedIndices().c_str()));
 
         list<pair<string, string> > lstParametersSVD = (*itSVD).getListParameters();
         list<pair<string, string> >::iterator itLstParametersSVD;
@@ -104,9 +103,9 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
         string strParamName = (*itLstParametersSVD).first;
         string strParamValue = (*itLstParametersSVD).second;
         QDomElement delemParameter;
-        delemParameter = ddocConfig.createElement(QString.fromStdString(strParamName.c_str()));
+        delemParameter = ddocConfig.createElement(QString::fromStdString(strParamName.c_str()));
         delemStateVariable.appendChild(delemParameter);
-        QDomText dtxtParamValue = ddocConfig.createTextNode(QString.fromStdString(strParamValue.c_str()));
+        QDomText dtxtParamValue = ddocConfig.createTextNode(QString::fromStdString(strParamValue.c_str()));
         delemParameter.appendChild(dtxtParamValue);
         }
     }
@@ -115,7 +114,7 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
 //TODO CHECK THIS
 //We don't have a direct mapping between DOMLSSerializer or DOMWriter to QtXML
 // Need to investigate this a bit further, but the idea would be using directly QDomDocument to write
-#if XERCES_VERSION_MAJOR >= 3
+/*#if XERCES_VERSION_MAJOR >= 3
     DOMLSSerializer* theSerializer = ((DOMImplementationLS*)impl)->createLSSerializer();
     if (theSerializer->getDomConfig()->canSetParameter(XMLUni::fgDOMWRTDiscardDefaultContent, true))
     theSerializer->getDomConfig()->setParameter(XMLUni::fgDOMWRTDiscardDefaultContent, true);
@@ -130,7 +129,12 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
     if (theSerializer->canSetFeature(XMLUni::fgDOMWRTFormatPrettyPrint, true))
     theSerializer->setFeature(XMLUni::fgDOMWRTFormatPrettyPrint, true);
 #endif
+*/
+    QFile myFormTarget(QString::fromStdString(strFileName.c_str()));
+    QTextStream output(&myFormTarget);
+    delemConfig.save(output, 4);
 
+    /*
     XMLFormatTarget *myFormTarget = new LocalFileFormatTarget(strFileName.c_str());
 
 #if XERCES_VERSION_MAJOR >= 3
@@ -147,13 +151,13 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
 #endif
     }
     catch (const XMLException& toCatch) {
-    char* message = QString.fromStdString(toCatch.getMessage());
+    char* message = QString::fromStdString(toCatch.getMessage());
     cerr << "Exception message is: \n"
          << message << "\n";
     XMLString::release(&message);
     }
     catch (const DOMException& toCatch) {
-    char* message = QString.fromStdString(toCatch.msg);
+    char* message = QString::fromStdString(toCatch.msg);
     cerr << "Exception message is: \n"
          << message << "\n";
     XMLString::release(&message);
@@ -166,7 +170,7 @@ bool ClsDataClientConfigWriter::saveConfig(string strFileName, list<ClsDataClien
 
     theSerializer->release();
     delete myFormTarget;
-
+*/
 
     return true;
 
